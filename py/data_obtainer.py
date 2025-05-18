1eae227326a7d847075b2a5b24e97c4cfa2daac3
Makoto ISHIHARA <evakichi@be.wakwak.com>
2025-05-18 20:16:33 +0900

    本番直前。

--- py/data_obtainer.py
+++ py/data_obtainer.py
@@ -8,6 +8,13 @@ from datetime import timedelta
 import mplfinance as mpf
 import japanize_matplotlib  
 import pathlib
+import ssl
+import shutil
+from smtplib import SMTP_SSL
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.utils import formatdate
+from email.mime.application import MIMEApplication
 
 class TokenTaker:
 
@@ -170,20 +177,20 @@ class Main:
 
         working_day = list()
         for cal in calender_taker.calendar["trading_calendar"]:
-            if cal["HolidayDivision"] not in ["0"]:
+            if cal["HolidayDivision"] not in ["0","3"]:
                 working_day.append(dt.datetime.strptime(cal["Date"], '%Y-%m-%d'))
         current = today
         for day in working_day:
             if day <= today:
                 previous = current
                 current = day
-        #print(current,previous)
-        self.golden_cross = list()
-        self.dead_cross = list()
+        print(current,previous)
+        self.sell_timing = list()
+        self.buy_timing = list()
         for package in packages:
             df = package["Data"]
             df["Rolling5"] = df["Close"].fillna(0).rolling(5).mean()
-            df["Rolling15"] = df["Close"].fillna(0).rolling(15).mean()
+            df["Rolling25"] = df["Close"].fillna(0).rolling(25).mean()
             df["Rolling150"] = df["Close"].fillna(0).rolling(150).mean()
             #print(tmp_df)
             df = df[df["Date"] <= current]
@@ -193,23 +200,25 @@ class Main:
             if df.shape[0] == 2:
                 #print(package["Code"])
                 df = df.set_index("Date")
-                if df.loc[current]["Rolling5"] > df.loc[current]["Close"] and df.loc[previous]["Close"] > df.loc[previous]["Rolling5"]:
+                if df.loc[current]["Rolling25"] > df.loc[current]["Close"] and df.loc[previous]["Close"] > df.loc[previous]["Rolling25"]:
+                    slope_rolling25 = df.loc[current]["Rolling25"]-df.loc[previous]["Rolling25"]
                     slope_rolling5 = df.loc[current]["Rolling5"]-df.loc[previous]["Rolling5"]
                     slope_close = df.loc[current]["Close"]-df.loc[previous]["Close"]
-                    index = (slope_rolling5 - slope_close)/((df.loc[current]["Volume"]+df.loc[previous]["Volume"])/2)
+                    index = (slope_rolling25 - slope_close)/((df.loc[current]["Volume"]+df.loc[previous]["Volume"])/2)
                     index = index / df.loc[current]["Close"]
-                    dead_cross = {"BrandCode":package["Brand"]["Code"],"Index":index,"Close":df.loc[current]["Close"],"Data":package["Data"]}
-                    self.dead_cross.append(dead_cross)
-                    #print("Dead cross",index)
+                    buy_timing = {"BrandCode":package["Brand"]["Code"],"Index":index,"Close":df.loc[current]["Close"],"Data":package["Data"]}
+                    self.buy_timing.append(buy_timing)
+                    #print("Sell timing",index)
                     #print(df)
-                if df.loc[current]["Close"] > df.loc[current]["Rolling5"] and df.loc[previous]["Rolling5"] > df.loc[previous]["Close"]:
+                if df.loc[current]["Close"] > df.loc[current]["Rolling25"] and df.loc[previous]["Rolling25"] > df.loc[previous]["Close"]:
+                    slope_rolling25 = df.loc[current]["Rolling25"]-df.loc[previous]["Rolling25"]
                     slope_rolling5 = df.loc[current]["Rolling5"]-df.loc[previous]["Rolling5"]
                     slope_close = df.loc[current]["Close"]-df.loc[previous]["Close"]
-                    index = (slope_close - slope_rolling5)/((df.loc[current]["Volume"]+df.loc[previous]["Volume"])/2)
+                    index = (slope_close - slope_rolling25)/((df.loc[current]["Volume"]+df.loc[previous]["Volume"])/2)
                     index = index / df.loc[current]["Close"]
-                    golden_cross = {"BrandCode":package["Brand"]["Code"],"Index":index,"Close":df.loc[current]["Close"],"Data":package["Data"]}
-                    self.golden_cross.append(golden_cross)
-                    #print("Golden cross",index)
+                    sell_timing = {"BrandCode":package["Brand"]["Code"],"Index":index,"Close":df.loc[current]["Close"],"Data":package["Data"]}
+                    self.sell_timing.append(sell_timing)
+                    #print("Buy timing",index)
                     #print(df)
             else:
                 pass
@@ -219,82 +228,83 @@ class Main:
         brand_code = list()
         index = list()
         close = list()
-        for golden_cross in self.golden_cross:
-            brand_code.append(golden_cross["BrandCode"])
-            index.append(golden_cross["Index"])
-            close.append(golden_cross["Close"])
+        for sell_timing in self.sell_timing:
+            brand_code.append(sell_timing["BrandCode"])
+            index.append(sell_timing["Index"])
+            close.append(sell_timing["Close"])
         data = {"BrandCode":brand_code,"Index":index,"Close":close}
-        golden_cross_df = pd.DataFrame(data,columns=["BrandCode","Index","Close"])
-        golden_cross_df = golden_cross_df.sort_values("Index", ascending=False)
-        golden_cross_less_than_500 = golden_cross_df[golden_cross_df["Close"]< 500.0].head(3)
-        golden_cross_between_500_and_3000 = golden_cross_df[golden_cross_df["Close"] >= 500.0]
-        golden_cross_between_500_and_3000 = golden_cross_between_500_and_3000[golden_cross_between_500_and_3000["Close"] < 3000.0].head(3)
-        golden_cross_between_3000_and_30000 = golden_cross_df[golden_cross_df["Close"] >= 3000.0]
-        golden_cross_between_3000_and_30000 = golden_cross_between_3000_and_30000[golden_cross_between_3000_and_30000["Close"] < 30000.0].head(3)
-        golden_cross_over_30000 = golden_cross_df[golden_cross_df["Close"] >= 30000.0].head(3)
-        print("Golden cross < 500.0")
-        print(golden_cross_less_than_500)
-        print("500 <= Golden cross < 3000.0")
-        print(golden_cross_between_500_and_3000)
-        print("3000.0 <= Golden cross < 30000.0")
-        print(golden_cross_between_3000_and_30000)
-        print("Golden cross >= 30000.0")
-        print(golden_cross_over_30000)
+        sell_timing_df = pd.DataFrame(data,columns=["BrandCode","Index","Close"])
+        sell_timing_df = sell_timing_df.sort_values("Index", ascending=False)
+        sell_timing_less_than_500 = sell_timing_df[sell_timing_df["Close"]< 500.0].head(3)
+        sell_timing_between_500_and_3000 = sell_timing_df[sell_timing_df["Close"] >= 500.0]
+        sell_timing_between_500_and_3000 = sell_timing_between_500_and_3000[sell_timing_between_500_and_3000["Close"] < 3000.0].head(3)
+        sell_timing_between_3000_and_30000 = sell_timing_df[sell_timing_df["Close"] >= 3000.0]
+        sell_timing_between_3000_and_30000 = sell_timing_between_3000_and_30000[sell_timing_between_3000_and_30000["Close"] < 30000.0].head(3)
+        sell_timing_over_30000 = sell_timing_df[sell_timing_df["Close"] >= 30000.0].head(3)
+        print("Sell timing < 500.0")
+        print(sell_timing_less_than_500)
+        print("500 <= Sell timing < 3000.0")
+        print(sell_timing_between_500_and_3000)
+        print("3000.0 <= Sell timing < 30000.0")
+        print(sell_timing_between_3000_and_30000)
+        print("Sell timing >= 30000.0")
+        print(sell_timing_over_30000)
 
         #print("Dead Cross")
         brand_code = list()
         index = list()
         close = list()
-        for dead_cross in self.dead_cross:
-            brand_code.append(dead_cross["BrandCode"])
-            index.append(dead_cross["Index"])
-            close.append(dead_cross["Close"])
+        for buy_timing in self.buy_timing:
+            brand_code.append(buy_timing["BrandCode"])
+            index.append(buy_timing["Index"])
+            close.append(buy_timing["Close"])
         data = {"BrandCode":brand_code,"Index":index,"Close":close}
-        dead_cross_df = pd.DataFrame(data,columns=["BrandCode","Index","Close"])
-        dead_cross_df = dead_cross_df.sort_values("Index", ascending=False)
-        dead_cross_less_than_500 = dead_cross_df[dead_cross_df["Close"]< 500.0].head(3)
-        dead_cross_between_500_and_3000 = dead_cross_df[dead_cross_df["Close"] >= 500.0]
-        dead_cross_between_500_and_3000 = dead_cross_between_500_and_3000[dead_cross_between_500_and_3000["Close"] < 3000.0].head(3)
-        dead_cross_between_3000_and_30000 = dead_cross_df[dead_cross_df["Close"] >= 3000.0]
-        dead_cross_between_3000_and_30000 = dead_cross_between_3000_and_30000[dead_cross_between_3000_and_30000["Close"] < 30000.0].head(3)
-        dead_cross_over_30000 = dead_cross_df[dead_cross_df["Close"] >= 30000.0].head(3)
-        print("Dead cross < 500.0")
-        print(dead_cross_less_than_500)
-        print("500.0 <= Dead cross < 3000.0")
-        print(dead_cross_between_500_and_3000)
-        print("3000.0 <= Dead cross < 30000.0")
-        print(dead_cross_between_3000_and_30000)
-        print("Dead cross >= 30000.0")
-        print(dead_cross_over_30000)
+        buy_timing_df = pd.DataFrame(data,columns=["BrandCode","Index","Close"])
+        buy_timing_df = buy_timing_df.sort_values("Index", ascending=False)
+        buy_timing_less_than_500 = buy_timing_df[buy_timing_df["Close"]< 500.0].head(3)
+        buy_timing_between_500_and_3000 = buy_timing_df[buy_timing_df["Close"] >= 500.0]
+        buy_timing_between_500_and_3000 = buy_timing_between_500_and_3000[buy_timing_between_500_and_3000["Close"] < 3000.0].head(3)
+        buy_timing_between_3000_and_30000 = buy_timing_df[buy_timing_df["Close"] >= 3000.0]
+        buy_timing_between_3000_and_30000 = buy_timing_between_3000_and_30000[buy_timing_between_3000_and_30000["Close"] < 30000.0].head(3)
+        buy_timing_over_30000 = buy_timing_df[buy_timing_df["Close"] >= 30000.0].head(3)
+        print("Buy timing < 500.0")
+        print(buy_timing_less_than_500)
+        print("500.0 <= Buy timing < 3000.0")
+        print(buy_timing_between_500_and_3000)
+        print("3000.0 <= Buy timing < 30000.0")
+        print(buy_timing_between_3000_and_30000)
+        print("Buy timing >= 30000.0")
+        print(buy_timing_over_30000)
         now = dt.datetime.now().strftime("%Y%m%d-%H%M%S")
         dir_path = pathlib.Path(os.path.join("/tmp/",now))
         dir_path.mkdir()
 
         results = list()
-        result = {"Kind":"500円未満の売り時銘柄TOP3","KindEnglish":"Dead cross less than 500 Yen","Data":dead_cross_less_than_500}
+        result = {"Kind":"500円未満の買い時銘柄TOP3","KindEnglish":"Buy timing less than 500 Yen","Data":buy_timing_less_than_500}
         results.append(result)
-        result = {"Kind":"500円以上3,000円未満の売り時銘柄TOP3","KindEnglish":"Dead cross between 500 and 3,000 Yen","Data":dead_cross_between_500_and_3000}
+        result = {"Kind":"500円以上3,000円未満の買い時銘柄TOP3","KindEnglish":"Buy timing between 500 and 3,000 Yen","Data":buy_timing_between_500_and_3000}
         results.append(result)
-        result = {"Kind":"3,000円以上30,000円未満の売り時銘柄TOP3","KindEnglish":"Dead cross between 3,000 and 30,000 Yen","Data":dead_cross_between_3000_and_30000}
+        result = {"Kind":"3,000円以上30,000円未満の買い時銘柄TOP3","KindEnglish":"Buy timing between 3,000 and 30,000 Yen","Data":buy_timing_between_3000_and_30000}
         results.append(result)
-        result = {"Kind":"30,000円以上の売り時銘柄TOP3","KindEnglish":"Dead cross over 30,000 Yen","Data":dead_cross_over_30000}
+        result = {"Kind":"30,000円以上の買い時銘柄TOP3","KindEnglish":"Buy timing over 30,000 Yen","Data":buy_timing_over_30000}
         results.append(result)
 
-        result = {"Kind":"500円未満の買い時銘柄TOP3","KindEnglish":"Golden cross less than 500 Yen","Data":golden_cross_less_than_500}
+        result = {"Kind":"500円未満の売り時銘柄TOP3","KindEnglish":"Sell timing less than 500 Yen","Data":sell_timing_less_than_500}
         results.append(result)
-        result = {"Kind":"500円以上3,000円未満の買い時銘柄TOP3","KindEnglish":"Golden cross between 500 and 3,000 Yen","Data":golden_cross_between_500_and_3000}
+        result = {"Kind":"500円以上3,000円未満の売り時銘柄TOP3","KindEnglish":"Sell timing between 500 and 3,000 Yen","Data":sell_timing_between_500_and_3000}
         results.append(result)
-        result = {"Kind":"3,000円以上30,000円未満の買い時銘柄TOP3","KindEnglish":"Golden cross between 3,000 and 30,000 Yen","Data":golden_cross_between_3000_and_30000}
+        result = {"Kind":"3,000円以上30,000円未満の売り時銘柄TOP3","KindEnglish":"Sell timing between 3,000 and 30,000 Yen","Data":sell_timing_between_3000_and_30000}
         results.append(result)
-        result = {"Kind":"30,000円以上の買い時銘柄TOP3","KindEnglish":"Golden cross over 30,000 Yen","Data":golden_cross_over_30000}
+        result = {"Kind":"30,000円以上の売り時銘柄TOP3","KindEnglish":"Sell timing over 30,000 Yen","Data":sell_timing_over_30000}
         results.append(result)
 
         self.print_image(dir_path,packages,current,results)
+        self.send_mail(dir_path,packages,current,results)
 
     def print_image(self,dir_path,packages,current,results):
         three_monthes_ago = current - timedelta(days=90)
+        print(current,three_monthes_ago)
         plotstyle = mpf.make_mpf_style(base_mpf_style='default', rc={"font.family":'IPAexGothic',"font.size":'8'})
-        print(results)
         for result in results:
             print(result["Kind"])
             brand_codes = result["Data"]["BrandCode"]
@@ -305,12 +315,60 @@ class Main:
                         df = df[df["Date"] <= current]
                         df = df[df["Date"] > three_monthes_ago]
                         df = df.set_index("Date")
-                        series = [mpf.make_addplot(df["Close"],type="line")]
-                        file_path = os.path.join(dir_path,package["Brand"]["Code"]+"_"+package["Brand"]["CompanyNameEnglish"]+".png")
-                        mpf.plot(df,type="candle",volume=True,mav=(5,10,15),title=package["Brand"]["CompanyName"],addplot=series,style=plotstyle,savefig=dict(fname=file_path,dpi=1600))
+                        series = [mpf.make_addplot(df["Close"],type="line",markersize=0.5)]
+                        file_path = os.path.join(dir_path,package["Brand"]["Code"]+"_"+package["Brand"]["CompanyNameEnglish"].replace('/','_')+".png")
+                        mpf.plot(df,type="candle",volume=True,mav=(5,25),title=package["Brand"]["CompanyName"],addplot=series,style=plotstyle,savefig=dict(fname=file_path,dpi=800))
 
-    def send_mail(self,dir_path,current,results):
-        os.getenv()
-        pass
 
+    def send_mail(self,dir_path,packages,current,results):
+        
+        message_header = MIMEMultipart()
+        message_header["Subject"] = current.strftime("%Y-%m-%d-%H:%M:%S")+"の株価情報"
+        message_header["From"] = os.environ.get('KABUROBO_KUN_MAIL_FROM')
+        message_header["To"] = os.environ.get('KABUROBO_KUN_MAIL_TO')
+        message_header['Date'] = formatdate()
+
+        message = ""
+        for result in results:
+            message += f"{result['Kind']}({result['KindEnglish']})\n"
+            data = result["Data"]
+            print(data)
+            if data.shape[0] == 0:
+                message += "該当無し。\n"
+            for rank in range(data.shape[0]):
+                for package in packages:
+                    if package["Brand"]["Code"] == data.iat[rank,0]:
+                        message += f"第{str(rank+1)}位(No. {str(rank+1)}):\n"
+                        message += f"\tCode = {package['Brand']['Code']}:{package['Brand']['CompanyName']}({package['Brand']['CompanyNameEnglish']})\n\t\t Index = {data.iat[rank,1]} 前回の終値(previous close) = {data.iat[rank,2]}\n"
+            message += "\n"
+
+        print (message)
+
+        message_text = MIMEText(message, 'plain', 'utf-8')
+        message_header.attach(message_text)
+
+        attachment_files = list()
+        zip_path = str(dir_path) + '.zip'        
+        shutil.make_archive(dir_path, format='zip', root_dir=dir_path)
+        attachment_files.append(zip_path)
+        shutil.rmtree(dir_path)
+
+
+        if attachment_files:
+            for attachment_file in attachment_files:
+                with open(attachment_file, 'rb') as file_pointer:
+                    attachment = MIMEApplication(file_pointer.read())
+                    attachment.add_header(
+                        "Content-Disposition",
+                        "attachment",
+                        filename=os.path.basename(attachment_file)
+                    )
+                    message_header.attach(attachment)
+
+        #server = SMTP_SSL(os.getenv('KABUROBO_KUN_MAIL_HOST'), 465, context=ssl.create_default_context())
+        #server.login(os.getenv('KABUROBO_KUN_MAIL_ACCOUNT'), os.getenv('KABUROBO_KUN_MAIL_PASSWORD'))
+        #server.send_message(message_header)
+
+        # 閉じる
+        #server.quit()
 main = Main()
